60904f631ad340a8bed71decd9f0bb7a18af0a40
jessicaelee <jessica.e.lee.93@gmail.com>
2020-01-29 11:46:18 -0800

    set up base templates and other templates

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
+
+toolbar = DebugToolbarExtension(app)
+
+app.debug = False
+
+app.config['SECRET_KEY'] = '<Rockinn Richard>'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
