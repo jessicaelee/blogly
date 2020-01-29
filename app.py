c42476cf2edd234207527d36c0a1ee7670b9e115
Paige Godfrey <paagodfrey@gmail.com>
2020-01-29 15:26:24 -0800

    setup skeleton routes

--- app.py
+++ app.py
@@ -2,13 +2,16 @@
 
 from flask import Flask, request, redirect, render_template
 from models import db, connect_db, User
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
-toolbar = DebugToolbarExtension(app)
+app = Flask(__name__)
+# app.config['SECRET_KEY'] = "never-tell!"
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-app.debug = False
+# debug = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
-app.config['SECRET_KEY'] = '<Rockinn Richard>'
+# app.debug = False
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -17,3 +20,52 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def root():
+
+    return redirect('/users')
+
+@app.route('/users')
+def show_users():
+
+    title = 'Users'
+    header = title
+    return render_template('users.html', title=title, header=header)
+
+@app.route('/users/new')
+def add_new_user():
+
+    title = 'Create a user'
+    header = title
+    return render_template('create_user.html', title=title, header=header)
+
+@app.route('/users/new', methods=["POST"])
+def process_new_user():
+
+    # request.form ...
+    return redirect('/users/new')
+
+@app.route('/users/<int: user-id>')
+def show_user_detail():
+
+    return render_template('user_detail.html')
+
+@app.route('/users/<int: user-id>/edit')
+def edit_user_detail():
+
+    return render_template('user_edit.html')
+
+@app.route('/users/<int: user-id>/edit', methods=["POST"])
+def process_edit_user_detail():
+
+    # request.form ...
+    return redirect('/users')
+
+@app.route('/users/<int: user-id>/delete', methods=["POST"])
+def process_delete_user():
+
+    # request.form ...
+    return redirect('/users') # maybe somewhere else ?
+
+
