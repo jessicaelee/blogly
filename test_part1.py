e5929ede11972efb1ddda393f8e9c66f59ff00b5
jessicaelee <jessica.e.lee.93@gmail.com>
2020-01-30 12:04:11 -0800

    add tests

--- test_part1.py
+++ test_part1.py
@@ -1,14 +1,21 @@
 from unittest import TestCase
 from app import app
 from flask import session
+from models import db, connect_db, User
 
 app.config['TESTING'] = True
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 class UserTests(TestCase):
 
+    def setUp(self):
+      self.client = app.test_client()
+      User.query.delete() #FIX THIS - TABLE ISN'T WORKING PROPERLY
+      db.session.commit()
+      
     def test_root_redirection(self):
-          with app.test_client() as client:
+          with self.client as client:
 
             resp = client.get('/', follow_redirects=True)
             html = resp.get_data(as_text=True)
@@ -17,19 +24,24 @@ class UserTests(TestCase):
             self.assertIn('<h1> Users </h1>', html)
 
     def test_show_added_user(self):
-          with app.test_client() as client:
+          with self.client as client:
 
             resp = client.post('/users/new', 
                             data={'first-name': 'test_first', 'last-name': 'test_last'}, follow_redirects=True)
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('<a href="/users/10">test_first test_last</a>', html)
+            self.assertIn('test_first test_last', html)
 
     def test_check_deleted_user(self):
-        with app.test_client() as client:
+        with self.client as client:
 
-            resp = client.post('/users/<int:user_id>/delete', follow_redirects=True)
+            create_user = client.post('/users/new', 
+                            data={'first-name': 'delete_first', 'last-name': 'delete_last'}, follow_redirects=True)
 
-             
+            resp = client.post('/users/34/delete', follow_redirects=True) #FIX NUMBER/ID REFERENCE 
+            html = resp.get_data(as_text=True)      
 
+            self.assertNotIn('delete_first delete_last', html)
+
+            # import pdb; pdb.set_trace()
